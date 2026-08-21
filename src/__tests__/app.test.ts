07d99ac585d413faebe5a4bb7def599cb1319d37
Mattar17 <mattar17x@gmail.com>
2026-08-21 11:41:23 +0300

    first integration test

---
+++ src/__tests__/app.test.ts
@@ -0,0 +1,13 @@
+import request from "supertest";
+
+import app from "@/index.js";
+import { describe, test, expect } from "@jest/globals";
+
+describe("GET /api/test", () => {
+  test("should return 200", async () => {
+    const res = await request(app)
+      .post("/reset-app-password")
+      .send({ password: "1234" });
+    expect(res.statusCode).toBe(200);
+  });
+});
